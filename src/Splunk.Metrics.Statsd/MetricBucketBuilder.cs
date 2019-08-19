36710df6be4731439440e3ce2600a0622ec650e8
Adam Surgenor <adam@surgenor.me>
2019-08-19 11:54:20 +0100

    Add support for custom dimensions
    Dimensions can be specified as the bucket builder is created, or per metric.

--- src/Splunk.Metrics.Statsd/MetricBucketBuilder.cs
+++ src/Splunk.Metrics.Statsd/MetricBucketBuilder.cs
@@ -1,5 +1,4 @@
-using System;
-using System.Collections.Specialized;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Splunk.Metrics.Statsd
@@ -10,46 +9,60 @@ namespace Splunk.Metrics.Statsd
         private readonly string _prefix;
         private readonly bool _ensureLowercasedMetricNames;
         private readonly bool _supportDimensions;
-        private readonly NameValueCollection _defaultDimensions;
+        private readonly IEnumerable<KeyValuePair<string, string>> _defaultDimensions;
 
         public MetricBucketBuilder(
             IEnvironment environment,
-            string prefix = null, 
+            string prefix = null,
             bool ensureLowercasedMetricNames = true,
-            bool supportDimensions = true)
+            bool supportDimensions = true,
+            IEnumerable<KeyValuePair<string, string>> additionalDimensions = null)
         {
             _environment = environment;
             _prefix = prefix;
             _ensureLowercasedMetricNames = ensureLowercasedMetricNames;
             _supportDimensions = supportDimensions;
-            _defaultDimensions = GenerateDefaultDimensions();
+            _defaultDimensions = additionalDimensions == null 
+                ? GenerateDefaultDimensions()
+                : GenerateDefaultDimensions().Concat(additionalDimensions);
         }
-        
-        public string Build(string metricType, string name, string value)
+
+        public string Build(string metricType, string name, string value,
+            IEnumerable<KeyValuePair<string, string>> additionalDimensions = null)
         {
             var extendedMetric = GenerateMetricBucketName(name)
                                  + ":" + value
                                  + "|" + metricType
-                                 + GenerateDimensionsForExtendedMetrics();
+                                 + GenerateDimensionsForExtendedMetrics(additionalDimensions);
             return _ensureLowercasedMetricNames ? extendedMetric.ToLowerInvariant() : extendedMetric;
         }
 
-        private string GenerateDimensionsForExtendedMetrics() => 
-            _defaultDimensions != null && _supportDimensions  ? "|#" + GenerateDimensions(_defaultDimensions) : string.Empty;
+        private string GenerateDimensionsForExtendedMetrics(
+            IEnumerable<KeyValuePair<string, string>> additionalDimensions = null)
+        {
+            if ((_defaultDimensions == null && additionalDimensions == null) || !_supportDimensions)
+                return string.Empty;
 
-        private string GenerateMetricBucketName(string name) => 
-            string.IsNullOrEmpty(_prefix) || _supportDimensions  ? name : _prefix + "." + name;
+            return _defaultDimensions != null && additionalDimensions != null
+                ? "|#" + GenerateDimensions(_defaultDimensions) + "," + GenerateDimensions(additionalDimensions)
+                : "|#" + GenerateDimensions(_defaultDimensions) + GenerateDimensions(additionalDimensions);
+        }
 
-        private static string GenerateDimensions(NameValueCollection dimensions) => 
-            string.Join(",", dimensions.AllKeys.Select(dimension => $"{dimension}:{dimensions[dimension]}"));
-        
-        private NameValueCollection GenerateDefaultDimensions() =>
+        private string GenerateMetricBucketName(string name) =>
+            string.IsNullOrEmpty(_prefix) || _supportDimensions ? name : _prefix + "." + name;
+
+        private static string GenerateDimensions(IEnumerable<KeyValuePair<string, string>> dimensions) =>
+            dimensions == null
+                ? string.Empty
+                : string.Join(",", dimensions.Select(dimension => $"{dimension.Key}:{dimension.Value}"));
+
+        private IEnumerable<KeyValuePair<string, string>> GenerateDefaultDimensions() =>
             !_supportDimensions
                 ? null
-                : new NameValueCollection
-                {
-                    { "instance", _environment.GetMachineName() },
-                    { "namespace", _prefix }
-                };
+                : new Dictionary<string, string>
+                    {
+                        {"instance", _environment.GetMachineName()},
+                        {"namespace", _prefix}
+                    };
     }
 }
