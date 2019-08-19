eaa684f5d4d2ae49f09d41d275e3eba45a466608
Adam Surgenor <adam@surgenor.me>
2019-08-19 12:45:10 +0100

    Dimensions support added to StatsPublisher

--- src/Splunk.Metrics.Abstractions/IStatsPublisher.cs
+++ src/Splunk.Metrics.Abstractions/IStatsPublisher.cs
@@ -1,21 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Splunk.Metrics.Abstractions
 {
     public interface IStatsPublisher
     {
-        Task GaugeAsync(string bucket, double value);
-        void Gauge(string bucket, double value);
+        Task GaugeAsync(string bucket, double value, IEnumerable<KeyValuePair<string,string>> additionalDimensions = null);
+        void Gauge(string bucket, double value, IEnumerable<KeyValuePair<string,string>> additionalDimensions = null);
         
-        IDisposable BeginTiming(string bucket);
+        IDisposable BeginTiming(string bucket, IEnumerable<KeyValuePair<string,string>> additionalDimensions = null);
         
-        Task TimingAsync(string bucket, long durationMilliseconds);
-        Task<T> TimingAsync<T>(string bucket, Func<Task<T>> func);
-        void Timing(string bucket, long durationMilliseconds);
-        T Timing<T>(string bucket, Func<T> func);
+        Task TimingAsync(string bucket, long durationMilliseconds, IEnumerable<KeyValuePair<string,string>> additionalDimensions = null);
+        Task<T> TimingAsync<T>(string bucket, Func<Task<T>> func, IEnumerable<KeyValuePair<string,string>> additionalDimensions = null);
+        void Timing(string bucket, long durationMilliseconds, IEnumerable<KeyValuePair<string,string>> additionalDimensions = null);
+        T Timing<T>(string bucket, Func<T> func, IEnumerable<KeyValuePair<string,string>> additionalDimensions = null);
         
-        Task IncrementAsync(string bucket, long count = 1L);
-        void Increment(string bucket, long count = 1L);
+        Task IncrementAsync(string bucket, long count = 1L, IEnumerable<KeyValuePair<string,string>> additionalDimensions = null);
+        void Increment(string bucket, long count = 1L, IEnumerable<KeyValuePair<string,string>> additionalDimensions = null);
     }
 }
